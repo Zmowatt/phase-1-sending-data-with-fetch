2ebfd99d3336f562c880614e3e646818fd11fcc4
Zachary Mowatt <zamowatt@gmail.com>
2025-03-10 21:44:09 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
-// Add your code here
-function submitData(name, email, id) {
-    const data = {
-        name: name,
-        email: email,
-        id: id,
-    };    
+// // Add your code here
 
-    const configObject = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify(data),
-};
+function submitData(name, email) {
 
-return fetch("http://localhost:3000/users", configObject)
-.then(function (response){
-    return response.json();
-})
-.then(function(data) {
-    const id = data.id;
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+        })
+    .then(res => res.json())
+    .then(function (data) {
 
-    const newId = document.createElement("p");
-    newId.textContent = `ID: ${id}`;
-    document.querySelector("body").appendChild(newId);
+        const id = data.id;
+        const newId = document.createElement('p');
+        newId.textContent = `ID: ${id}`;
+        document.querySelector("body").appendChild(newId) 
+        console.log(data)
+    })
+        .catch(function (error){
+        alert("Panic! Panic! Panic!")
+        
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = `Error: ${error}`;
+        document.querySelector("body").appendChild(errorMessage)
+    })
+}
 
-    console.log(data)
-})
-.catch(function(error) {
-
-    const errorMessage = document.createElement("p")
-    errorMessage.textContent = `Error: ${error}`;
-    document.querySelector("body").appendChild(errorMessage);
-
-    console.log(error.message);
-});
-};
+submitData("Zach Mowatt", "MrMime93@aol.com")
